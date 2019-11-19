70e4be7c3e620bc9c9c8c4efcc4877833b49ecd6
Xandromus <xanderrapstine@gmail.com>
2019-11-19 11:52:41 -0600

    added in readme and files, removed commit language, changed title

---
+++ 01-Class-Content/04-Web-APIs/02-Challenge/Master/assets/js/scores.js
@@ -0,0 +1,29 @@
+function printHighscores() {
+  // either get scores from localstorage or set to empty array
+  var highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
+
+  // sort highscores by score property in descending order
+  highscores.sort(function(a, b) {
+    return b.score - a.score;
+  });
+
+  highscores.forEach(function(score) {
+    // create li tag for each high score
+    var liTag = document.createElement("li");
+    liTag.textContent = score.initials + " - " + score.score;
+
+    // display on page
+    var olEl = document.getElementById("highscores");
+    olEl.appendChild(liTag);
+  });
+}
+
+function clearHighscores() {
+  window.localStorage.removeItem("highscores");
+  window.location.reload();
+}
+
+document.getElementById("clear").onclick = clearHighscores;
+
+// run function when page loads
+printHighscores();
