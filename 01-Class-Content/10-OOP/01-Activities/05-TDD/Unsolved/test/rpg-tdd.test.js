5413ca4b17fc98dc92cda3c22c6d13ff52a04b56
gchoi2u <gchoi@2u.com>
2020-04-07 13:05:30 -0400

    added activity 5 of 10 sync session

---
+++ 01-Class-Content/10-OOP/01-Activities/05-TDD/Unsolved/test/rpg-tdd.test.js
@@ -0,0 +1,14 @@
+const Character = require("../rpg-tdd")
+
+test('creates a character object', () => {
+  // create a new character with a `name`, `strength`, and `hitpoints` values
+
+  // create a test that checks if the `name` is a `string`
+
+  // create a test that checks if the `strength` and `hitpoints` are `numbers`
+
+  // create a test that checks if the `hitpoints` is greater than the `strength`
+  
+});
+
+// Bonus: write a test that will check if the character is alive or not
