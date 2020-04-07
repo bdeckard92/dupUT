31a132e8be6bce74de2bf9887a605447321077f9
gchoi2u <gchoi@2u.com>
2020-04-07 14:36:58 -0400

    modified activity 3 of 10 sync session

--- 01-Class-Content/10-OOP/01-Activities/03-RPG-Prototypes/README.md
+++ 01-Class-Content/10-OOP/01-Activities/03-RPG-Prototypes/README.md
@@ -4,7 +4,7 @@ In this activity, you will generate RPG characters using constructors and protot
 
 ## Instructions
 
-* Open [Unsolved/rpg-prototypes.js](Unsolved/rpg-prototypes.js). 
+* Open the [Unsolved/rpg-prototypes.js](Unsolved/rpg-prototypes.js) file.
 
 * Create a constructor function called `Character` that takes in 3 arguments and assigns them to the appropriate keys:
 
@@ -12,12 +12,12 @@ In this activity, you will generate RPG characters using constructors and protot
 
   * `strength`
 
-  * `hp` (or `hitpoints`)
+  * `hitpoints`
 
 * Create 3 prototype methods:
 
-  * `PrintStats()`: Function which prints all of the stats for a character
+  * `printStats()`: Function which prints all of the stats for a character
 
-  * `IsAlive()`: Function which prints whether or not this character is alive by determining whether their hitpoints are above or below zero.
+  * `isAlive()`: Function which prints whether or not this character is alive by determining whether their hitpoints are above or below zero.
 
-  * `Attack()`: Function which takes in a second character and subtracts this character's strength from their hitpoints.
+  * `attack()`: Function which takes in a second character and subtracts this character's strength from their hitpoints.
--- 01-Class-Content/10-OOP/01-Activities/03-RPG-Prototypes/Solved/rpg-prototypes.js
+++ 01-Class-Content/10-OOP/01-Activities/03-RPG-Prototypes/Solved/rpg-prototypes.js
@@ -1,4 +1,4 @@
-// Create a constructor function called `Character` that takes in 3 arguments: name, strength, hitpoints 
+// Create a constructor function called `Character` that takes in 3 arguments: name, strength, hitpoints
 function Character(name, strength, hitpoints) {
   this.name = name;
   this.strength = strength;
@@ -7,32 +7,38 @@ function Character(name, strength, hitpoints) {
 
 // Create a prototype method called `printStats()` which prints all of the stats for a character
 Character.prototype.printStats = function() {
-	console.log("Name: " + this.name + "\nStrength: " + this.strength + "\nHitPoints: " + this.hitpoints);
-	console.log("\n-------------\n");
+  console.log(
+    'Name: ' +
+      this.name +
+      '\nStrength: ' +
+      this.strength +
+      '\nHitPoints: ' +
+      this.hitpoints
+  );
+  console.log('\n-------------\n');
 };
 
-// Create a prototype method called `isAlive()` which prints whether or not this character is alive 
+// Create a prototype method called `isAlive()` which prints whether or not this character is alive
 // by determining whether their hitpoints are above or below zero
 Character.prototype.isAlive = function() {
-	if (this.hitpoints > 0) {
-		console.log(this.name + " is still alive!");
-		console.log("\n-------------\n");
-	} else {
-		console.log(this.name + " has died!");
-	}
+  if (this.hitpoints > 0) {
+    console.log(this.name + ' is still alive!');
+    console.log('\n-------------\n');
+  } else {
+    console.log(this.name + ' has died!');
+  }
 };
 
-// Create a prototype method called `attack()` which takes in a second character 
+// Create a prototype method called `attack()` which takes in a second character
 // and subtracts this character's strength from their hitpoints
 Character.prototype.attack = function(opponent) {
-	opponent.hitpoints -= this.strength;
-	console.log(this.name + " attacked " + opponent.name + "!!")
-	console.log("\n-------------\n");
+  opponent.hitpoints -= this.strength;
+  console.log(this.name + ' attacked ' + opponent.name + '!!');
+  console.log('\n-------------\n');
 };
 
-// Creates two unique characters using the "character" constructor
-let warrior = new Character("Crusher", 10, 75);
-let rogue = new Character("Dodger", 20, 50);
+let warrior = new Character('Crusher', 10, 75);
+let rogue = new Character('Dodger', 20, 50);
 
 warrior.printStats();
 rogue.printStats();
