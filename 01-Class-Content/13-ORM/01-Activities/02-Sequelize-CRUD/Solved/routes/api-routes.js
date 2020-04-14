1e73dcab35c75422e79ee3685e72b20b8b6a40bb
gchoi2u <gchoi@2u.com>
2020-04-14 13:21:32 -0400

    modified activity 2 of 13 sync sessions

--- 01-Class-Content/13-ORM/01-Activities/02-Sequelize-CRUD/Solved/routes/api-routes.js
+++ 01-Class-Content/13-ORM/01-Activities/02-Sequelize-CRUD/Solved/routes/api-routes.js
@@ -1,35 +1,27 @@
 const router = require('express').Router();
-// Requiring our Todo model
 const Todo = require('../models/todo');
 
 // Routes
 // =============================================================
+
 // GET route for getting all of the todos
 router.get('/api/todos', (req, res) => {
-  // findAll returns all entries for a table when used with no options
   Todo.findAll({}).then(dbTodo => {
-    // We have access to the todos as an argument inside of the callback function
     res.json(dbTodo);
   });
 });
 
-// POST route for saving a new todo
+// POST route for saving a new todo. We can create todo with the data in 'req.body'
 router.post('/api/todos', (req, res) => {
-  console.log(req.body);
-  // create takes an argument of an object describing the item we want to
-  // insert into our table. In this case we just we pass in an object with a text
-  // and complete property (req.body)
   Todo.create({
     text: req.body.text,
     complete: req.body.complete
   }).then(dbTodo => {
-    // We have access to the new todo as an argument inside of the callback function
     res.json(dbTodo);
   });
 });
 
-// DELETE route for deleting todos. We can get the id of the todo we want to delete from
-// req.params.id
+// DELETE route for deleting a todo. We can get the id of the todo to be deleted from 'req.params.id'
 router.delete('/api/todos/:id', (req, res) => {
   Todo.destroy({
     where: {
@@ -40,7 +32,7 @@ router.delete('/api/todos/:id', (req, res) => {
   });
 });
 
-// PUT route for updating todos. We can get the updated todo from req.body
+// PUT route for updating a todo. We can update todo with the data in req.body
 router.put('/api/todos/:id', (req, res) => {
   Todo.update(
     {
--- 01-Class-Content/13-ORM/01-Activities/02-Sequelize-CRUD/Unsolved/routes/api-routes.js
+++ 01-Class-Content/13-ORM/01-Activities/02-Sequelize-CRUD/Unsolved/routes/api-routes.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
-// Requiring our Todo model
 const Todo = require('../models/todo');
 
 // Routes
@@ -7,19 +6,20 @@ const Todo = require('../models/todo');
 
 // GET route for getting all of the todos
 router.get('/api/todos', (req, res) => {
-  // Write code here to retrieve all of the todos from the database and res.json them
-  // back to the user
+  // Write code here to retrieve all of the todos from the database and then `res.json` them back to the user
 });
 
-// POST route for saving a new todo. We can create todo with the data in req.body
+// POST route for saving a new todo. We can create todo with the data in 'req.body'
 router.post('/api/todos', (req, res) => {
-  // Write code here to create a new todo and save it to the database
-  // and then res.json back the new todo to the user
+  // Write code here to create a new todo and then `res.json` back the new todo to the user
 });
 
-// DELETE route for deleting todos. We can get the id of the todo to be deleted from
-// req.params.id
-router.delete('/api/todos/:id', (req, res) => {});
+// DELETE route for deleting a todo. We can get the id of the todo to be deleted from 'req.params.id'
+router.delete('/api/todos/:id', (req, res) => {
+  // Write code here to delete a todo and then `res.json` back the result back to the user
+});
 
-// PUT route for updating todos. We can get the updated todo data from req.body
-router.put('/api/todos/:id', (req, res) => {});
+// PUT route for updating a todo. We can update todo with the data in req.body
+router.put('/api/todos/:id', (req, res) => {
+  // Write code here to update a todo and then `res.json` back the result back to the user
+});
