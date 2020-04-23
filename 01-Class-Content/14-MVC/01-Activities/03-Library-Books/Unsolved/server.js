60c986dcbe95dfd22699e412b1ed03fdda9c3e08
gchoi2u <gchoi@2u.com>
2020-04-23 10:21:25 -0400

    added activity 3 for 14 sync session

---
+++ 01-Class-Content/14-MVC/01-Activities/03-Library-Books/Unsolved/server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+// Set Handlebars as the default template engine here
+
+const books = [
+  {
+    title: 'Love You Forever',
+    read: false,
+    author: 'Robert Munsch'
+  },
+  {
+    title: 'The Giving Tree',
+    read: false,
+    author: 'Shel Silverstein'
+  },
+  {
+    title: 'Where the Red Fern Grows',
+    read: true,
+    author: 'Wilson Rawls'
+  },
+  {
+    title: 'The Fault in Our Stars',
+    read: true,
+    author: 'John Green'
+  },
+  {
+    title: 'Out of My Mind',
+    read: false,
+    author: 'Sally Engelfried'
+  },
+  {
+    title: 'Wonder',
+    read: false,
+    author: 'Barbara Schultz'
+  }
+];
+
+app.get('/', (req, res) => {
+  // Send the books to the index handlebars file as an object
+});
+
+app.listen(PORT, () => {
+  console.log('App listening on PORT ' + PORT);
+});
