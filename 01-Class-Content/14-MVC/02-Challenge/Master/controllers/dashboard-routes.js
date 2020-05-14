78e64b37ff91afe41979f28ef2ea04f7eb97dfa1
Xandromus <xanderrapstine@gmail.com>
2020-05-13 22:49:38 -0500

    adding in solution code but it is broken

---
+++ 01-Class-Content/14-MVC/02-Challenge/Master/controllers/dashboard-routes.js
@@ -0,0 +1,46 @@
+const router = require("express").Router();
+const { Post } = require("../models/");
+const withAuth = require("../utils/auth");
+
+router.get("/", withAuth, (req, res) => {
+    Post.findAll({
+      where: {
+        userId: req.session.userId
+      }
+    })
+      .then(posts => {
+        res.render("all-posts-admin", {
+          layout: "dashboard",
+          posts
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        res.redirect("login");
+      });
+  });
+
+  router.get("/new", withAuth, (req, res) => {
+    res.render("new-post", {
+      layout: "dashboard"
+    });
+  });
+  
+  router.get("/edit/:id", withAuth, (req, res) => {
+    Post.findByPk(req.params.id)
+      .then(post => {
+        if (post) {
+          res.render("edit-post", {
+            layout: "dashboard",
+            post
+          });
+        } else {
+          res.status(404).end();
+        }
+      })
+      .catch(err => {
+        res.status(500).json(err);
+      });
+  });
+  
+module.exports = router;
---
+++ 01-Class-Content/14-MVC/02-Challenge/Master/controllers/home-routes.js
@@ -0,0 +1,58 @@
+const router = require("express").Router();
+const { Post, Comment, User } = require("../models/");
+
+// get all posts for homepage
+router.get("/", (req, res) => {
+    Post.findAll({
+      include: [User]
+    })
+      .then(posts => {
+        res.render("all-posts", { posts });
+      })
+      .catch(err => {
+        res.status(500).json(err);
+      });
+  });
+  
+  // get single post
+  router.get("/post/:id", (req, res) => {
+    Post.findByPk(req.params.id, {
+      include: [
+        User,
+        {
+          model: Comment,
+          include: [User]
+        }
+      ]
+    })
+      .then(post => {
+        if (post) {
+          res.render("single-post", { post });
+        } else {
+          res.status(404).end();
+        }
+      })
+      .catch(err => {
+        res.status(500).json(err);
+      });
+  });
+
+  router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+      res.redirect('/');
+      return;
+    }
+  
+    res.render('login');
+  });
+
+  router.get("/signup", (req, res) => {
+    if (req.session.loggedIn) {
+      res.redirect('/');
+      return;
+    }
+  
+    res.render('signup');
+  });
+
+  module.exports = router;
