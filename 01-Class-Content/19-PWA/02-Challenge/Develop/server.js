fc76866665a8ffe392a275914880b065f3f4b395
Xandromus <xanderrapstine@gmail.com>
2020-06-09 16:50:47 -0500

    change port to 3001

--- 01-Class-Content/19-PWA/02-Challenge/Develop/server.js
+++ 01-Class-Content/19-PWA/02-Challenge/Develop/server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = 3001;
 
 const app = express();
 
