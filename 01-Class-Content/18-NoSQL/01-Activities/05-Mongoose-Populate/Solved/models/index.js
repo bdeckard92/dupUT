dbaef4ee0362f562895031f308bb4c7e5ebf0f54
gchoi2u <gchoi@2u.com>
2020-06-10 11:45:40 -0400

    modified 05 activity per review

--- 01-Class-Content/18-NoSQL/01-Activities/05-Mongoose-Populate/Solved/models/index.js
+++ 01-Class-Content/18-NoSQL/01-Activities/05-Mongoose-Populate/Solved/models/index.js
@@ -1,6 +1,6 @@
 // Exporting an object containing all of our models
 
 module.exports = {
-  Note: require("./Note"),
-  User: require("./User")
+  Note: require('./Note'),
+  User: require('./User')
 };
