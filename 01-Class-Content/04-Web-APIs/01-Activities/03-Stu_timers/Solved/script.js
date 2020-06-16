06736cf96f352c43795a5c23a330cabd81ec4aeb
gchoi2u <gchoi@2u.com>
2020-06-16 12:07:53 -0400

    simplified 03 timer activity

--- 01-Class-Content/04-Web-APIs/01-Activities/03-Stu_timers/Solved/script.js
+++ 01-Class-Content/04-Web-APIs/01-Activities/03-Stu_timers/Solved/script.js
@@ -1,43 +1,26 @@
-var poem = "Some say the world will end in 🔥, Some say in ice. From what I’ve tasted of desire, I hold with those who favor fire. But if it had to perish twice, I think I know enough of hate. To say that for destruction ice, Is also great, And would suffice.";
-var words = poem.split(" ");
+var poem =
+  'Some say the world will end in 🔥, Some say in ice. From what I’ve tasted of desire, I hold with those who favor fire. But if it had to perish twice, I think I know enough of hate. To say that for destruction ice, Is also great, And would suffice.';
+var words = poem.split(' ');
 
-var mainEl = document.getElementById("main");
-var readEl = document.getElementById("read");
-var timerEl = document.getElementById("countdown");
-var bodyEl = document.createElement("div");
+var timerEl = document.getElementById('countdown');
+var mainEl = document.getElementById('main');
+var startBtn = document.getElementById('start');
 
 var count = 0;
 
-var wordsPerMillisecond = prompt("How many words per millisecond would you like to read?");
-
-function prepareRead() {
+// Timer that counts down from 5
+function countdown() {
   var timeLeft = 5;
 
-  var timeInterval = setInterval(function () {
-    timerEl.textContent = timeLeft + " seconds remaining";
+  var timeInterval = setInterval(function() {
+    timerEl.textContent = timeLeft + ' seconds remaining';
     timeLeft--;
 
     if (timeLeft === 0) {
-      timerEl.textContent = "";
-      speedRead();
+      timerEl.textContent = '';
       clearInterval(timeInterval);
     }
-
   }, 1000);
 }
 
-
-function speedRead() {
-
-  var poemInterval = setInterval(function () {
-    if (words[count] === undefined) {
-      clearInterval(poemInterval);
-    } else {
-      mainEl.textContent = words[count];
-      count++;
-    }
-
-  }, wordsPerMillisecond);
-}
-
-prepareRead();
+startBtn.onclick = countdown;
