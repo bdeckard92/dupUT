b1f5a57f71e5c1552beec0afa96ca7a516e24816
gchoi2u <gchoi@2u.com>
2020-06-16 13:51:51 -0400

    fixes issue 163 modified 03 activity using timers

--- 01-Class-Content/04-Web-APIs/01-Activities/03-Stu_timers/Solved/script.js
+++ 01-Class-Content/04-Web-APIs/01-Activities/03-Stu_timers/Solved/script.js
@@ -1,26 +1,44 @@
-var poem =
-  'Some say the world will end in 🔥, Some say in ice. From what I’ve tasted of desire, I hold with those who favor fire. But if it had to perish twice, I think I know enough of hate. To say that for destruction ice, Is also great, And would suffice.';
-var words = poem.split(' ');
-
 var timerEl = document.getElementById('countdown');
 var mainEl = document.getElementById('main');
 var startBtn = document.getElementById('start');
 
-var count = 0;
+var message =
+  'Congratulations! Now you are prepared to tackle the Challenge this week! Good luck!';
+var words = message.split(' ');
 
 // Timer that counts down from 5
 function countdown() {
   var timeLeft = 5;
 
+  // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
   var timeInterval = setInterval(function() {
-    timerEl.textContent = timeLeft + ' seconds remaining';
-    timeLeft--;
-
-    if (timeLeft === 0) {
+    if (timeLeft > 1) {
+      timerEl.textContent = timeLeft + ' seconds remaining';
+      timeLeft--;
+    } else if (timeLeft === 1) {
+      timerEl.textContent = timeLeft + ' second remaining';
+      timeLeft--;
+    } else {
       timerEl.textContent = '';
       clearInterval(timeInterval);
+      displayMessage();
     }
   }, 1000);
+}
+
+// Displays the message one word at a time
+function displayMessage() {
+  var wordCount = 0;
+
+  // Uses the `setInterval()` method to call a function to be executed every 300 milliseconds
+  var msgInterval = setInterval(function() {
+    if (words[wordCount] === undefined) {
+      clearInterval(msgInterval);
+    } else {
+      mainEl.textContent = words[wordCount];
+      wordCount++;
+    }
+  }, 300);
 }
 
 startBtn.onclick = countdown;
