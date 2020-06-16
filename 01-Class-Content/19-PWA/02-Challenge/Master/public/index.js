794fccf645b3f70af882c7bb2d6f6d876ae0f423
markjnkim <mark.jn.kim@gmail.com>
2020-06-16 12:47:24 -0700

    align service-worker.js with module

--- 01-Class-Content/19-PWA/02-Challenge/Master/public/index.js
+++ 01-Class-Content/19-PWA/02-Challenge/Master/public/index.js
@@ -1,12 +1,3 @@
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
-      .then((reg) => {
-        console.log('Service worker registered.', reg);
-      });
-  });
-}
-
 let transactions = [];
 let myChart;
 
