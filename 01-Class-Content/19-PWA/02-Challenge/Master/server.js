893dbb3ad92988fe6100d753239c3a7c2311a91d
markjnkim <mark.jn.kim@gmail.com>
2020-07-08 12:43:33 -0700

    modify env to match instructions

--- 01-Class-Content/19-PWA/02-Challenge/Master/server.js
+++ 01-Class-Content/19-PWA/02-Challenge/Master/server.js
@@ -3,7 +3,8 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const port = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 
 const app = express();
 
@@ -15,7 +16,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useFindAndModify: false
 });
@@ -24,5 +25,5 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${port}!`);
+  console.log(`App running on port ${PORT}!`);
 });
