6060946e69ff93b29382d09fbe4196fafd2e6800
gchoi2u <gchoi@2u.com>
2020-09-01 09:04:20 -0400

    adding seed file

--- 01-Class-Content/13-ORM/01-Activities/01-Sequelize-Library/Solved/app/seeds/books.js
+++ 01-Class-Content/13-ORM/01-Activities/01-Sequelize-Library/Solved/app/seeds/books.js
@@ -3,13 +3,41 @@ const sequelize = require('../config/connection');
 
 const bookdata = [
   {
-    title: 'Crime and Punishment',
-    author: 'Fyodor Dostoevsky',
-    genre: 'Crime Fiction',
-    pages: 430
+    title: 'Pachinko',
+    author: 'Min Jin Lee',
+    genre: 'Fiction',
+    pages: 496
+  },
+  {
+    title: 'A Girl Like That',
+    author: 'Tanaz Bhatena',
+    genre: 'Fiction',
+    pages: 384
+  },
+  {
+    title: 'How the Garcia Girls Lost Their Accents',
+    author: 'Julia Alvarez',
+    genre: 'Fiction',
+    pages: 336
+  },
+  {
+    title: 'Saving Ruby King: A Novel',
+    author: 'Catherine Adel West',
+    genre: 'Fiction',
+    pages: 284
   }
 ];
 
-const seedBooks = () => Book.bulkCreate(bookdata);
-
-module.exports = seedBooks;
+sequelize
+  .sync({ force: true })
+  .then(() => {
+    return Book.bulkCreate(bookdata);
+  })
+  .then(dbBookData => {
+    console.log('Books seeded!');
+    process.exit(0);
+  })
+  .catch(err => {
+    console.log(err);
+    process.exit(1);
+  });
