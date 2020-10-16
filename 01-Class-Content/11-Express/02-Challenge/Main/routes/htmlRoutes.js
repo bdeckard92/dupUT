51d06513866f8dea7e4682b3c13469e3cf8d38cc
Xandromus <xanderrapstine@gmail.com>
2020-10-16 10:30:56 -0500

    add in refactored starter and solution code

--- 01-Class-Content/11-Express/02-Challenge/Main/routes/htmlRoutes.js
+++ 01-Class-Content/11-Express/02-Challenge/Main/routes/htmlRoutes.js
@@ -1,14 +1,14 @@
-var path = require("path");
-var router = require("express").Router();
+const path = require('path');
+const router = require('express').Router();
 
 // "/notes" responds with the notes.html file
-router.get("/notes", function(req, res) {
-  res.sendFile(path.join(__dirname, "../public/notes.html"));
+router.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/notes.html'));
 });
 
 // All other routes respond with the index.html file
-router.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "../public/index.html"));
+router.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 module.exports = router;
