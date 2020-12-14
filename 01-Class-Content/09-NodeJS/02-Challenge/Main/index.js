4fefb80ff83fe58deaf59d52b083d060ea9b8953
gchoi2u <gchoi@2u.com>
2020-12-14 11:10:00 -0500

    added comments to Solved and Unsolved code in 09 Challenge

--- 01-Class-Content/09-NodeJS/02-Challenge/Main/index.js
+++ 01-Class-Content/09-NodeJS/02-Challenge/Main/index.js
@@ -1,68 +1,72 @@
-const fs = require("fs");
-const path = require("path");
-const inquirer = require("inquirer");
-const generateMarkdown = require("./utils/generateMarkdown");
+// Imported required packages
+const fs = require('fs');
+const path = require('path');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 
+// Array of questions for user input
 const questions = [
   {
-    type: "input",
-    name: "github",
-    message: "What is your GitHub username?"
-  },{
-    type: "input",
-    name: "email",
-    message: "What is your email address?"
+    type: 'input',
+    name: 'github',
+    message: 'What is your GitHub username?'
   },
   {
-    type: "input",
-    name: "title",
+    type: 'input',
+    name: 'email',
+    message: 'What is your email address?'
+  },
+  {
+    type: 'input',
+    name: 'title',
     message: "What is your project's name?"
   },
   {
-    type: "input",
-    name: "description",
-    message: "Please write a short description of your project"
+    type: 'input',
+    name: 'description',
+    message: 'Please write a short description of your project'
   },
   {
-    type: "list",
-    name: "license",
-    message: "What kind of license should your project have?",
-    choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"]
+    type: 'list',
+    name: 'license',
+    message: 'What kind of license should your project have?',
+    choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None']
   },
   {
-    type: "input",
-    name: "installation",
-    message: "What command should be run to install dependencies?",
-    default: "npm i"
+    type: 'input',
+    name: 'installation',
+    message: 'What command should be run to install dependencies?',
+    default: 'npm i'
   },
   {
-    type: "input",
-    name: "test",
-    message: "What command should be run to run tests?",
-    default: "npm test"
+    type: 'input',
+    name: 'test',
+    message: 'What command should be run to run tests?',
+    default: 'npm test'
   },
   {
-    type: "input",
-    name: "usage",
-    message: "What does the user need to know about using the repo?",
+    type: 'input',
+    name: 'usage',
+    message: 'What does the user need to know about using the repo?'
   },
   {
-    type: "input",
-    name: "contributing",
-    message: "What does the user need to know about contributing to the repo?",
+    type: 'input',
+    name: 'contributing',
+    message: 'What does the user need to know about contributing to the repo?'
   }
 ];
 
+// Function to write README file using the user input
 function writeToFile(fileName, data) {
   return fs.writeFileSync(path.join(process.cwd(), fileName), data);
 }
 
+// Function to initialize app
 function init() {
-  inquirer.prompt(questions)
-  .then((inquirerResponses) => {
-    console.log("Generating README...");
-    writeToFile("README.md", generateMarkdown({...inquirerResponses}));
-  })
+  inquirer.prompt(questions).then(inquirerResponses => {
+    console.log('Generating README...');
+    writeToFile('README.md', generateMarkdown({ ...inquirerResponses }));
+  });
 }
 
 init();
--- 01-Class-Content/09-NodeJS/02-Challenge/Main/utils/generateMarkdown.js
+++ 01-Class-Content/09-NodeJS/02-Challenge/Main/utils/generateMarkdown.js
@@ -1,30 +1,33 @@
+// Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  if (license !== "None") {
-    return `![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)`
+  if (license !== 'None') {
+    return `![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)`;
   }
-  return ''
+  return '';
 }
 
+// Create a function that returns the license link
+// If there is no license, return an empty string
 function renderLicenseLink(license) {
-  if (license !== "None") {
-    return (
-      `\n* [License](#license)\n`
-    )
+  if (license !== 'None') {
+    return `\n* [License](#license)\n`;
   }
-  return ''
+  return '';
 }
 
+// Create a function that returns the license section of README
+// If there is no license, return an empty string
 function renderLicenseSection(license) {
-  if (license !== "None") {
-    return (
-      `## License
+  if (license !== 'None') {
+    return `## License
 
-This project is licensed under the ${license} license.`
-    )
+This project is licensed under the ${license} license.`;
   }
-  return ''
+  return '';
 }
 
+// Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 ${renderLicenseBadge(data.license)}
@@ -73,7 +76,11 @@ ${data.test}
 
 ## Questions
 
-If you have any questions about the repo, open an issue or contact me directly at ${data.email}. You can find more of my work at [${data.github}](https://github.com/${data.github}/).
+If you have any questions about the repo, open an issue or contact me directly at ${
+    data.email
+  }. You can find more of my work at [${data.github}](https://github.com/${
+    data.github
+  }/).
 
 `;
 }
