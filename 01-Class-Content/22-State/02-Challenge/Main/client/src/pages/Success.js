45dd46d8136beb431d888121378d59b4e1285c23
Alex Rosenkranz <alex.rosenkranz@gmail.com>
2021-01-25 11:36:23 -0500

    feat(FSFO-1727): removes if-env and switches to apollo client

--- 01-Class-Content/22-State/02-Challenge/Main/client/src/pages/Success.js
+++ 01-Class-Content/22-State/02-Challenge/Main/client/src/pages/Success.js
@@ -1,8 +1,8 @@
-import React, { useEffect } from "react";
-import { useMutation } from '@apollo/react-hooks';
-import Jumbotron from "../components/Jumbotron";
-import { ADD_ORDER } from "../utils/mutations";
-import { idbPromise } from "../utils/helpers";
+import React, { useEffect } from 'react';
+import { useMutation } from '@apollo/client';
+import Jumbotron from '../components/Jumbotron';
+import { ADD_ORDER } from '../utils/mutations';
+import { idbPromise } from '../utils/helpers';
 
 function Success() {
   const [addOrder] = useMutation(ADD_ORDER);
@@ -10,17 +10,17 @@ function Success() {
   useEffect(() => {
     async function saveOrder() {
       const cart = await idbPromise('cart', 'get');
-      const products = cart.map(item => item._id);
-      
+      const products = cart.map((item) => item._id);
+
       if (products.length) {
         const { data } = await addOrder({ variables: { products } });
         const productData = data.addOrder.products;
-    
+
         productData.forEach((item) => {
           idbPromise('cart', 'delete', item);
         });
       }
-        
+
       setTimeout(() => {
         window.location.assign('/');
       }, 3000);
@@ -33,15 +33,11 @@ function Success() {
     <div>
       <Jumbotron>
         <h1>Success!</h1>
-        <h2>
-          Thank you for your purchase!
-        </h2>
-        <h2>
-          You will now be redirected to the home page
-        </h2>
+        <h2>Thank you for your purchase!</h2>
+        <h2>You will now be redirected to the home page</h2>
       </Jumbotron>
     </div>
   );
-};
+}
 
 export default Success;
