49141afd0032f3f47b733dc59129b798e0f76d14
gchoi2u <gchoi@2u.com>
2021-01-26 13:23:31 -0500

    sync code formatting for lesson-file and main

--- 01-Class-Content/12-SQL/05-Module-Project/main/db/connection.js
+++ 01-Class-Content/12-SQL/05-Module-Project/main/db/connection.js
@@ -5,7 +5,7 @@ const db = mysql.createConnection({
   // Your MySQL username,
   user: 'root',
   // Your MySQL password
-  password: 'gchoi@Trilogy!',
+  password: '!',
   database: 'election'
 });
 
