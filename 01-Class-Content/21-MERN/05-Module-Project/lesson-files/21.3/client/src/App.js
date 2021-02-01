27dbb2fa0c1a66cf3fe1eddfb27c2f38f6fb9d66
Alex Rosenkranz <alex.rosenkranz@gmail.com>
2021-02-01 10:52:11 -0500

    feat(21.3): adds apollo client, removes if-env, general utility updates

--- 01-Class-Content/21-MERN/05-Module-Project/lesson-files/21.3/client/src/App.js
+++ 01-Class-Content/21-MERN/05-Module-Project/lesson-files/21.3/client/src/App.js
@@ -1,15 +1,25 @@
 import React from 'react';
-
-import { ApolloProvider } from '@apollo/react-hooks';
-import ApolloClient from 'apollo-boost';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 import Home from './pages/Home';
 
+// Construct our main GraphQL API endpoint
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
 const client = new ApolloClient({
-  uri: '/graphql'
+  // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+  link: httpLink,
+  cache: new InMemoryCache(),
 });
 
 function App() {
