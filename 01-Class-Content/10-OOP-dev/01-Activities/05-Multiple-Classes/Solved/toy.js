ee1a97c0f59c16e0dbad6f92131b55fb8bb2af6d
Tamar <mischegoss@gmail.com>
2021-04-09 12:05:44 -0400

    Fix indents

--- 01-Class-Content/10-OOP-dev/01-Activities/05-Multiple-Classes/Solved/toy.js
+++ 01-Class-Content/10-OOP-dev/01-Activities/05-Multiple-Classes/Solved/toy.js
@@ -1,20 +1,17 @@
-// Define class using class declaration
 class Toy {
-    // Initializes object
-    constructor(name, price, count) {
-      this.name = name;
-      this.price = price;
-      this.count = count;
-    }
+  constructor(name, price, count) {
+    this.name = name;
+    this.price = price;
+    this.count = count;
   }
-  // Creates two new instances of toy: Action Figure and Rare Toy
-  const toys = [
-    // New instance of will have the name 'Action Figure', price of 14.99 and count of 5
-    new Toy("Action Figure", 14.99, 5),
-    new Toy("Rare Toy", 17.99, 1)
-  ];
-  
-  module.exports = {
-    Toy,
-    toys
-  };
+}
+
+const toys = [
+  new Toy("Action Figure", 14.99, 5),
+  new Toy("Rare Toy", 17.99, 1)
+];
+
+module.exports = {
+  Toy,
+  toys
+};
