76476f993ab3f1f7f2d1bcec3430e69d638ad83c
John McCambridge <jmccamb@gmail.com>
2021-04-27 13:43:44 -0500

    Remove posix argument

--- 01-Class-Content/03-JavaScript/02-Challenge/Main/script.js
+++ 01-Class-Content/03-JavaScript/02-Challenge/Main/script.js
@@ -92,8 +92,7 @@ var upperCasedCharacters = [
 function getPasswordOptions() {
   // Variable to store length of password from user input
   var length = parseInt(
-    prompt('How many characters would you like your password to contain?'),
-    10
+    prompt('How many characters would you like your password to contain?')
   );
 
   // Conditional statement to check if password length is a number. Prompts end if this evaluates false
