d81ed3c92fc247dfed0dc2a86870bce1afe002ef
John McCambridge <jmccamb@gmail.com>
2021-06-07 15:50:39 -0500

    Update to ES6

--- 01-Class-Content/09-NodeJS/01-Activities/02-Inquirer/Solved/index.js
+++ 01-Class-Content/09-NodeJS/01-Activities/02-Inquirer/Solved/index.js
@@ -1,7 +1,7 @@
 // 1. funnyCase makes each letter in a string the opposite case as the one before
-var funnyCase = string => {
-  var newString = "";
-  for (var i = 0; i < string.length; i++) {
+const funnyCase = string => {
+  let newString = '';
+  for (let i = 0; i < string.length; i++) {
     if (i % 2 === 0) newString += string[i].toLowerCase();
     else newString += string[i].toUpperCase();
   }
@@ -14,18 +14,18 @@ console.log(funnyCase("You can't just do whatever you want all the time!"));
 // --------------------------------------------------------------------------
 
 // 2. Map lets you loop over an array and modify the elements inside
-var map = (arr, cb) => {
-  var result = [];
-  for (var index = 0; index < arr.length; index++) {
-    var currentElement = arr[index];
+const map = (arr, cb) => {
+  const result = [];
+  for (let index = 0; index < arr.length; index++) {
+    const currentElement = arr[index];
     result.push(cb(currentElement, index));
   }
   return result;
 };
 
-var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-var doubled = map(numbers, element => element * 2);
+const doubled = map(numbers, element => element * 2);
 
 // Prints `[ 2, 4, 6, 8, 10, 12, 14, 16, 18, 20 ]`
 console.log(doubled);
@@ -33,10 +33,10 @@ console.log(doubled);
 // --------------------------------------------------------------------------
 
 // 3. filter lets you loop over an array and remove elements
-var filter = (arr, cb) => {
-  var result = [];
-  for (var index = 0; index < arr.length; index++) {
-    var currentElement = arr[index];
+const filter = (arr, cb) => {
+  const result = [];
+  for (let index = 0; index < arr.length; index++) {
+    const currentElement = arr[index];
     if (cb(currentElement, index)) {
       result.push(currentElement);
     }
@@ -44,7 +44,7 @@ var filter = (arr, cb) => {
   return result;
 };
 
-var evenNumbers = filter(numbers, currentElement => currentElement % 2 === 0);
+const evenNumbers = filter(numbers, currentElement => currentElement % 2 === 0);
 
 // Prints `[ 2, 4, 6, 8, 10 ]`
 console.log(evenNumbers);
@@ -52,36 +52,36 @@ console.log(evenNumbers);
 // --------------------------------------------------------------------------
 
 // 4. netflixQueue is an object for managing your netflix queue
-var netflixQueue = {
+const netflixQueue = {
   queue: [
-    "Mr. Nobody",
-    "The Matrix",
-    "Eternal Sunshine of the Spotless Mind",
-    "Fight Club"
+    'Mr. Nobody',
+    'The Matrix',
+    'Eternal Sunshine of the Spotless Mind',
+    'Fight Club'
   ],
-  watchMovie: function() {
+  watchMovie() {
     this.queue.pop();
   },
-  addMovie: function(movie) {
+  addMovie(movie) {
     this.queue.unshift(movie);
   },
-  printQueue: function() {
-    var list = "";
-    for (var i = this.queue.length - 1; i >= 0; i--) {
-      var currentMovie = this.queue[i];
-      list += (this.queue.length - i) + ". " + currentMovie + "\n";
+  printQueue() {
+    let list = '';
+    for (let i = this.queue.length - 1; i >= 0; i--) {
+      const currentMovie = this.queue[i];
+      list += `${this.queue.length - i}. ${currentMovie}\n`;
     }
     console.log(list);
   }
 };
 
-console.log("Printing movie queue!\n");
+console.log('Printing movie queue!\n');
 netflixQueue.printQueue();
 netflixQueue.watchMovie();
-console.log("\nWatched a movie!\n");
-console.log("Printing movie queue!\n");
+console.log('\nWatched a movie!\n');
+console.log('Printing movie queue!\n');
 netflixQueue.printQueue();
-console.log("\nAdding a movie!\n");
-netflixQueue.addMovie("Black Swan");
-console.log("Printing movie queue!\n");
+console.log('\nAdding a movie!\n');
+netflixQueue.addMovie('Black Swan');
+console.log('Printing movie queue!\n');
 netflixQueue.printQueue();
