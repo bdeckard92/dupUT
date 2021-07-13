db2f2cf6d13113b7b67bd632e50038331c301eec
John McCambridge <jmccamb@gmail.com>
2021-07-13 10:44:35 -0500

    Add TODOs to unsolved

--- 01-Class-Content/06-Server-Side-APIs/01-Activities/01-Create_Fetch/Solved/assets/js/script.js
+++ 01-Class-Content/06-Server-Side-APIs/01-Activities/01-Create_Fetch/Solved/assets/js/script.js
@@ -2,14 +2,14 @@ var repoList = document.querySelector('ul');
 var fetchButton = document.getElementById('fetch-button');
 
 function getApi() {
-  // replace `octocat` with anyone else's GitHub username
+  // Replace `octocat` with anyone else's GitHub username
   var requestUrl = 'https://api.github.com/users/octocat/repos';
 
   fetch(requestUrl)
-    .then(function (response) {
+    .then(function(response) {
       return response.json();
     })
-    .then(function (data) {
+    .then(function(data) {
       for (var i = 0; i < data.length; i++) {
         var listItem = document.createElement('li');
         listItem.textContent = data[i].html_url;
--- 01-Class-Content/06-Server-Side-APIs/01-Activities/01-Create_Fetch/Unsolved/assets/js/script.js
+++ 01-Class-Content/06-Server-Side-APIs/01-Activities/01-Create_Fetch/Unsolved/assets/js/script.js
@@ -1,10 +1,10 @@
 var repoList = document.querySelector('ul');
 var fetchButton = document.getElementById('fetch-button');
 
-//getApi function is called when the fetchButton is clicked
+// `getApi` function is called when the `fetchButton` is clicked
 
 function getApi() {
-  // Insert the API url to get a list of your repos
+  // TODO: Insert the API url to get a list of your repos
   var requestUrl = '';
 
   fetch(requestUrl)
@@ -12,15 +12,15 @@ function getApi() {
       return response.json();
     })
     .then(function(data) {
-      //looping over the fetch response and inserting the URL of your repos into a list
+      // Looping over the fetch response and inserting the URL of your repos into a list
       for (var i = 0; i < data.length; i++) {
-        //Create a list element
+        // Create a list element
         var listItem = document.createElement('li');
 
-        //Set the text of the list element to the JSON response's .html_url property
+        // Set the text of the list element to the JSON response's .html_url property
         listItem.textContent = data[i].html_url;
 
-        //Append the li element to the id associated with the ul element.
+        // Append the li element to the id associated with the ul element.
         repoList.appendChild(listItem);
       }
     });
