a216823b84a5055a57f2d48d416db1797761760e
John McCambridge <jmccamb@gmail.com>
2021-08-04 11:13:04 -0500

    Fixes:
    - remove nested GG folder
    - copy over correct algos

--- 01-Class-Content/04-Web-APIs/03-Algorithms/02-odd-or-even/Unsolved/odd-or-even.js
+++
@@ -1,4 +0,0 @@
-// Write code to return the string "odd" if `num` is an odd number
-// and return the string "even" if `num` is an even number
-
-var oddOrEven = function(num) {};
--- 01-Class-Content/04-Web-APIs/03-Algorithms/03-log-nums/Solved/log-nums.js
+++
@@ -1,8 +0,0 @@
-// Write code to print all numbers from 1 to `num`
-// Assume `num` will be a positive number
-
-var logNums = function(num) {
-  for (var i = 1; i <= num; i++) {
-    console.log(i);
-  }
-};
