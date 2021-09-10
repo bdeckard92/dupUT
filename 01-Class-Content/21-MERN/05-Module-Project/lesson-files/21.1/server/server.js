3f241dd3f02a53ce3d1a7eb89be9783be89cc439
parkerfoshay <40288355+parkerfoshay@users.noreply.github.com>
2021-09-10 15:31:17 -0700

    updated apollo server

--- 01-Class-Content/21-MERN/05-Module-Project/lesson-files/21.1/server/server.js
+++ 01-Class-Content/21-MERN/05-Module-Project/lesson-files/21.1/server/server.js
@@ -6,12 +6,19 @@ const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-const server = new ApolloServer({
-  typeDefs,
-  resolvers
-});
 
-server.applyMiddleware({ app });
+async function startServer() {
+  const server = new ApolloServer({ 
+    typeDefs, 
+    resolvers, 
+    context: authMiddleware 
+  });
+  await server.start();
+  server.applyMiddleware({ app });
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+}
+
+startServer()
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -19,6 +26,5 @@ app.use(express.json());
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
 });
--- 01-Class-Content/21-MERN/05-Module-Project/lesson-files/21.2/server/server.js
+++ 01-Class-Content/21-MERN/05-Module-Project/lesson-files/21.2/server/server.js
@@ -8,13 +8,19 @@ const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: authMiddleware
-});
 
-server.applyMiddleware({ app });
+async function startServer() {
+  const server = new ApolloServer({ 
+    typeDefs, 
+    resolvers, 
+    context: authMiddleware 
+  });
+  await server.start();
+  server.applyMiddleware({ app });
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+}
+
+startServer()
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -22,6 +28,5 @@ app.use(express.json());
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
 });
